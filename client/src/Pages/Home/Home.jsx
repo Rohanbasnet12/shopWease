c98c7aa460f8112be698a5333a105b49bde3e357
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-11-23 13:11:41 +0530

    changed home UI

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -3,11 +3,19 @@ import homeIMG from "../../assets/homeIMG.jpeg";
 
 const Home = () => {
   return (
-    <div id="home" className="w-full h-screen bg-[#B6A28E]">
+    <div
+      id="home"
+      className="w-full h-screen bg-[#B6A28E]"
+      style={{ fontFamily: `"Roboto", sans-serif` }}
+    >
       <div className="home-wrapper container mx-auto pt-20">
         <div id="img_wrapper" className="border-2 rounded-xl">
           <img src={homeIMG} alt="Home image" />
         </div>
+        {/* Button */}
+        <button className="border bg-white rounded-3xl px-6 py-2 text-md font-medium shadow-lg hover:bg-[#E07B39] hover:text-white transition-all">
+          Buy Now
+        </button>
       </div>
     </div>
   );
