21eb6bc3f8f9e98ec0a757fe34e7e06ce88e8b21
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-11-23 13:02:01 +0530

    Styling home UI

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import homeIMG from "../../assets/homeIMG.jpeg";
 
 const Home = () => {
   return (
     <div id="home" className="w-full h-screen bg-[#B6A28E]">
-      <h1>Home</h1>
+      <div className="home-wrapper container mx-auto pt-20">
+        <div id="img_wrapper" className="border-2 rounded-xl">
+          <img src={homeIMG} alt="Home image" />
+        </div>
+      </div>
     </div>
   );
 };
