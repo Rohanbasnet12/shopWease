47abbd81fa3dd8e14d5204879320eb70074049ab
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-11-24 09:53:23 +0530

    Working on Home UI

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,21 +5,21 @@ const Home = () => {
   return (
     <div
       id="home"
-      className="w-full h-screen bg-[#B6A28E] pt-20"
+      className="w-full h-screen bg-[#B6A28E] pt-10"
       style={{ fontFamily: `"Roboto", sans-serif` }}
     >
-      <div className="home-wrapper container mx-auto overflow-hidden relative h-[80vh] rounded-3xl">
+      <div className="home-wrapper container mx-auto overflow-hidden relative rounded-3xl">
         {/* {Image} */}
-        <div className="img_wrapper">
+        <div className="img_wrapper h-[80vh] w-full -mt-10 overflow-hidden">
           <img
             src={homeImg}
             alt="Background Home Image"
-            className="w-full scale-y-150 transform origin-bottom -translate-y-40"
+            className="w-full h-full object-cover scale-x-150"
           />
         </div>
 
         {/* {Button} */}
-        <button className="border bg-white rounded-3xl px-6 py-2 text-md font-medium shadow-lg hover:bg-[#E07B39] hover:text-white transition-all absolute bottom-10 right-10">
+        <button className="border bg-white rounded-3xl px-6 py-2 text-md font-medium shadow-lg hover:bg-[#E07B39] hover:text-white transition-all absolute bottom-10 right-20">
           Buy Now
         </button>
       </div>
