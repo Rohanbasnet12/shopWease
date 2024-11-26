2ab6d2748162ca0021cd0babe69843f5629c2731
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-11-26 22:36:36 +0530

    Last commit of the day

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,47 +1,11 @@
 import React from "react";
 import homeImg from "../assets/homeImg.png";
+import Hero from "../components/Hero";
 
 const Home = () => {
   return (
-    <div
-      id="home"
-      className="w-full h-screen bg-[#B6A28E] pt-10"
-      style={{ fontFamily: `"Roboto", sans-serif` }}
-    >
-      <div className="home-wrapper container mx-auto overflow-hidden relative rounded-3xl">
-        {/* {Image} */}
-        <div className="img_wrapper h-[80vh] w-full -mt-10 overflow-hidden">
-          <img
-            src={homeImg}
-            alt="Background Home Image"
-            className="w-full h-full object-cover scale-x-110"
-          />
-        </div>
-
-        <div className="info-wrapper w-full lg:px-10 md:p-6 xl:px-12 p-3 flex justify-between items-center absolute bottom-4">
-          <div className="flex flex-col w-full gap-3">
-            <p className="text-white font-bold text-4xl uppercase">
-              shopWease Spring Collections
-            </p>
-
-            <div className="flex items-center justify-between">
-              <div className="flex flex-col items-start">
-                <span className="text-xm text-slate-100 font-thin">
-                  Find out best spring collection. Offering our best quality
-                  production.
-                </span>
-                <span className="text-xm text-slate-100 font-thin">
-                  Only at shopWease
-                </span>
-              </div>
-
-              <button className="border bg-white rounded-3xl px-6 py-2 text-md font-medium shadow-lg hover:bg-[#E07B39] hover:text-white transition-all">
-                Buy Now
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
+    <div id="home">
+      <Hero />
     </div>
   );
 };
