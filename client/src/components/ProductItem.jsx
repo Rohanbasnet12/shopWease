3745269e6e27aa4301d2a494086d7284a817b9cc
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-11-28 10:18:00 +0530

    Completed product card UI

--- client/src/components/ProductItem.jsx
+++ client/src/components/ProductItem.jsx
@@ -8,19 +8,18 @@ const ProductItem = ({ id, image, name, price }) => {
   return (
     <Link
       to={`/product/${id}`}
-      className="text-gray-600 cursor-pointer border-2 rounded-xl p-2"
+      className="text-gray-600 cursor-pointer border-2 border-rose-400/40 shadow-lg shadow-rose-400/20 rounded-xl p-2"
     >
       <div className="overflow-hidden">
+        {/* Image */}
         <img
           src={image[0]}
           alt={`${image} image`}
           className="hover:scale-110 transition ease-out rounded-tl-md rounded-tr-md"
         />
       </div>
-      <div
-        id="product-description"
-        className="flex flex-col items-start justify-between pt-4"
-      >
+      {/* Product Description */}
+      <div id="product-description" className="flex flex-col items-start pt-4">
         <p className="text-md font-bold leading-4">{name}</p>
         <span className="pt-2 text-sm font-medium">
           {currency} {price}
