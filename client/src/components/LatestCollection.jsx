39aa60b1a187c673d0e97056b9c695a6f8c68cf8
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-11-29 20:16:23 +0530

    Changing Navbar UI

--- client/src/components/LatestCollection.jsx
+++ client/src/components/LatestCollection.jsx
@@ -22,7 +22,7 @@ const LatestCollection = () => {
 
       <div
         id="product-item-wrapper"
-        className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4 gap-y-6"
+        className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-5 gap-y-6"
       >
         {latestProducts.map((item, index) => (
           <ProductItem
