8f1aa6a477dac7d6769ab74be111e88b18a85353
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-11-30 19:01:38 +0530

    Working on our policy section

---
+++ client/src/components/OurPolicy.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const OurPolicy = () => {
+  return (
+    <div id="ourpolicy">
+      <div id="ourpolicy-wrapper" className="border-2 border-black w-full h-40">
+        <h1>Our Policy</h1>
+      </div>
+    </div>
+  );
+};
+
+export default OurPolicy;
