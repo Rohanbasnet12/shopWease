ca9effd227d35e7813fb62fe1c8d991c27b5ddf2
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-11-30 10:48:01 +0530

    Working on footer sectionCopyright

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,9 +1,18 @@
 import React from "react";
 
 const Footer = () => {
+  const currentYear = new Date().getFullYear();
+
+  console.log(currentYear);
   return (
-    <div>
-      <h1>Footer</h1>
+    <div
+      id="footer"
+      className="flex items-center justify-center mt-14 py-8 px-3 md:px-8 lg:px-12 border-t-2"
+    >
+      <h1>
+        Copyright ©<span> {currentYear} </span>
+        Rohan Basnet - All Right Reserved.
+      </h1>
     </div>
   );
 };
