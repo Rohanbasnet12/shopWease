53739e085dc0ea34515f73e44cd4e4a6124e519f
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-12-01 19:08:39 +0530

    Changing our policy UI

--- client/src/components/OurPolicy.jsx
+++ client/src/components/OurPolicy.jsx
@@ -17,54 +17,49 @@ const OurPolicy = () => {
           "At shopWease, we are committed to transparency, privacy, and security."
         }
       />
+
       <div
-        id="ourpolicy-wrapper"
-        className=" w-[113%] h-[80vh] px-16 transform -translate-x-8 sm:-translate-x-10 -rotate-6 flex items-center justify-center"
-        style={{ background: "#B6A28E" }}
+        id="service-wrapper"
+        className="w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 gap-y-10"
       >
-        <div
-          id="service-wrapper"
-          className="w-full rotate-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6 gap-y-10"
-        >
-          <div className="box1 flex flex-col items-center justify-center">
-            <img
-              src={ExchangeIcon}
-              alt="Quality icon"
-              className="w-10 h-10 mb-3"
-            />
-            <h2 className="text-lg font-medium text-slate-800">
-              Easy Exchange Policy
-            </h2>
-            <p className="text-sm text-gray-200">
-              We offer hassle free exchange policy
-            </p>
-          </div>
-          <div className="box2 flex flex-col items-center justify-center">
-            <img
-              src={QualityIcon}
-              alt="Quality icon"
-              className="w-10 h-10 mb-3"
-            />
-            <h2 className="text-lg font-medium text-slate-800">
-              7 Days Return Policy
-            </h2>
-            <p className="text-sm text-gray-200">
-              We provide 7 days free return policy
-            </p>
-          </div>
-          <div className="box3 flex flex-col items-center justify-center">
-            <img
-              src={SupportIcon}
-              alt="Quality icon"
-              className="w-10 h-10 mb-3"
-            />
-            <h2 className="text-lg font-medium text-slate-800">
-              Best customer support
-            </h2>
-            <p className="text-sm text-gray-200">
-              we provide 24/7 customer support
-            </p>
-          </div>
+        <div className="box1 flex flex-col items-center justify-center">
+          <img
+            src={ExchangeIcon}
+            alt="Quality icon"
+            className="w-10 h-10 mb-3"
+          />
+          <h2 className="text-lg font-medium text-slate-800">
+            Easy Exchange Policy
+          </h2>
+          <p className="text-sm text-gray-400">
+            We offer hassle free exchange policy
+          </p>
+        </div>
+        <div className="box2 flex flex-col items-center justify-center">
+          <img
+            src={QualityIcon}
+            alt="Quality icon"
+            className="w-10 h-10 mb-3"
+          />
+          <h2 className="text-lg font-medium text-slate-800">
+            7 Days Return Policy
+          </h2>
+          <p className="text-sm text-gray-400">
+            We provide 7 days free return policy
+          </p>
+        </div>
+        <div className="box3 flex flex-col items-center justify-center">
+          <img
+            src={SupportIcon}
+            alt="Quality icon"
+            className="w-10 h-10 mb-3"
+          />
+          <h2 className="text-lg font-medium text-slate-800">
+            Best customer support
+          </h2>
+          <p className="text-sm text-gray-400">
+            we provide 24/7 customer support
+          </p>
         </div>
       </div>
     </div>
