77a33a049b216477a0d4fc0717aa6b9a49686c15
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-12-07 21:59:17 +0530

    Working on filter category section

--- client/src/pages/Collections.jsx
+++ client/src/pages/Collections.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import Title from "../components/Title";
+import ShopContext from "../context/ShopContext";
 
 const Collections = () => {
+  const { products } = useContext(ShopContext);
+  const [showFilter, setShowFilter] = useState(false);
+
   return (
     <div
       id="collection"
@@ -19,6 +23,33 @@ const Collections = () => {
         className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t"
       >
         {/* Filter functionality */}
+        <div className="min-w-60">
+          <p className="uppercase my-2 text-xl flex items-center cursor-pointer gap-2">
+            Filter
+          </p>
+          {/* Filter category */}
+          <div
+            className={`border border-gray-300 pl-5 mt-6 ${
+              showFilter ? "" : "hidden"
+            }`}
+          >
+            <p className="uppercase text-xs font-medium">Categories</p>
+            <div className="flex flex-col gap-2 text-sm font-light text-gray700">
+              <p className="flex gap-2">
+                <input type="checkbox" className="w-3" value={"Men"} />
+                Men
+              </p>
+              <p className="flex gap-2">
+                <input type="checkbox" className="w-3" value={"Women"} />
+                Women
+              </p>
+              <p className="flex gap-2">
+                <input type="checkbox" className="w-3" value={"Kids"} />
+                Kids
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
