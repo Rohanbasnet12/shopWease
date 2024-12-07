4883c5b3d261548052463b6b803392c8a83c7161
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-12-07 22:11:48 +0530

    Working on filter responsive behavior

--- client/src/pages/Collections.jsx
+++ client/src/pages/Collections.jsx
@@ -1,6 +1,7 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Title from "../components/Title";
-import ShopContext from "../context/ShopContext";
+import { ShopContext } from "../context/ShopContext";
+import DropDownIcon from "../assets/frontend_assets/dropdown_icon.png";
 
 const Collections = () => {
   const { products } = useContext(ShopContext);
@@ -22,18 +23,28 @@ const Collections = () => {
         id="collection-wrapper"
         className="flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t"
       >
-        {/* Filter functionality */}
+        {/* Filter Functionality */}
         <div className="min-w-60">
-          <p className="uppercase my-2 text-xl flex items-center cursor-pointer gap-2">
+          <p
+            onClick={() => {
+              setShowFilter(!showFilter);
+            }}
+            className="uppercase my-2 text-xl flex items-center cursor-pointer gap-2"
+          >
             Filter
+            <img
+              className={`h-3 pl-3 sm:hidden ${showFilter ? "rotate-90" : ""}`}
+              src={DropDownIcon}
+              alt="Drop Down Icon"
+            />
           </p>
-          {/* Filter category */}
+          {/* Filter Category */}
           <div
-            className={`border border-gray-300 pl-5 mt-6 ${
+            className={`border border-gray-300 pl-5 py-3 mt-6 ${
               showFilter ? "" : "hidden"
             }`}
           >
-            <p className="uppercase text-xs font-medium">Categories</p>
+            <p className="mb-3 uppercase text-xs font-medium">Categories</p>
             <div className="flex flex-col gap-2 text-sm font-light text-gray700">
               <p className="flex gap-2">
                 <input type="checkbox" className="w-3" value={"Men"} />
@@ -46,6 +57,28 @@ const Collections = () => {
               <p className="flex gap-2">
                 <input type="checkbox" className="w-3" value={"Kids"} />
                 Kids
+              </p>
+            </div>
+          </div>
+          {/* SubCategory Filter */}
+          <div
+            className={`border border-gray-300 pl-5 py-3 mt-6 ${
+              showFilter ? "" : "hidden"
+            }`}
+          >
+            <p className="mb-3 uppercase text-xs font-medium">Type</p>
+            <div className="flex flex-col gap-2 text-sm font-light text-gray700">
+              <p className="flex gap-2">
+                <input type="checkbox" className="w-3" value={"Topwear"} />
+                Topwear
+              </p>
+              <p className="flex gap-2">
+                <input type="checkbox" className="w-3" value={"Bottomwear"} />
+                Bottomwear
+              </p>
+              <p className="flex gap-2">
+                <input type="checkbox" className="w-3" value={"Winterwear"} />
+                Winterwear
               </p>
             </div>
           </div>
