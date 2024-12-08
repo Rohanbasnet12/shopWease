af0e3beaecc574bdff899e81f688b615f087ba87
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-12-08 17:30:31 +0530

    Working on product cards UI

--- client/src/pages/Collections.jsx
+++ client/src/pages/Collections.jsx
@@ -82,12 +82,15 @@ const Collections = () => {
           <div className="flex justify-between text-base sm:text-2xl mb-4">
             <Title title={"LATEST COLLECTIONS"} />
             {/* Product sort */}
-            <select className="border-2 border-gray-300 text-sm px-2">
+            <select className="border-2 border-gray-300 text-sm px-2 h-10 my-10">
               <option value="relavent">Sort by: Relavent</option>
               <option value="low-high">Sort by: Low to High</option>
               <option value="high-low">Sort by: High to Low</option>
             </select>
           </div>
+
+          {/* Map Products */}
+          <div className="grid grid-cols-2"></div>
         </div>
       </div>
     </div>
