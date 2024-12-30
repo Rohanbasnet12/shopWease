91d5169e89e36bbb944a5887cb0cf776720be196
Rohanbasnet12 <rohanbmyname@gmail.com>
2024-12-30 21:47:52 +0530

    Working on search bar location functionality

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -3,23 +3,25 @@ import { ShopContext } from "../context/ShopContext";
 
 const SearchBar = () => {
   const { search, setSearch } = useContext(ShopContext);
+  const [isClear, setIsClear] = useState(false);
 
   return (
     <>
       <div
         id="searchBar"
-        className="flex w-[60vw] sm:w-[300px] lg:w-[400px] xl:w-[500px] items-center justify-center gap-3 px-3 rounded-lg bg-[#B6A28E]/25 shadow-md border border-[#B6A28E]/80"
+        className="flex w-[65vw] sm:w-[300px] lg:w-[400px] xl:w-[500px] items-center justify-center gap-3 px-3 rounded-lg bg-[#B6A28E]/25 shadow-md border border-[#B6A28E]/80"
       >
+        <i className="fa-solid fa-magnifying-glass text-slate-800"></i>
         <input
           type="text"
           id="searchBarInput"
           name="searchBarInput"
-          className="outline-none w-full py-1 bg-transparent"
+          className="outline-none w-full py-1 pl-2 bg-transparent border-l-2 border-[#B6A28E]/80"
           placeholder="search..."
           value={search}
           onChange={(e) => setSearch(e.target.value)}
         />
-        <i className="fa-solid fa-magnifying-glass text-slate-800"></i>
+        {isClear && <i className="fa-solid fa-xmark text-slate-800" />}
       </div>
     </>
   );
