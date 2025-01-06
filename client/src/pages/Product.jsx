4ce3ffa4c620e45d697785b3d41a8fefb4dab9bd
Rohanbasnet12 <rohanbmyname@gmail.com>
2025-01-06 21:13:40 +0530

    Working on product page

--- client/src/pages/Product.jsx
+++ client/src/pages/Product.jsx
@@ -2,7 +2,11 @@ import React from "react";
 
 const Product = () => {
   return (
-    <div>
+    <div
+      id="product"
+      className="px-3 md:px-8 lg:px-20 mb-16 mt-20"
+      style={{ fontFamily: `"Roboto", sans-serif` }}
+    >
       <h1>Product</h1>
     </div>
   );
